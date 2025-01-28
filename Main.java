d37bdc7283d5a44ee3f59329ef3a734de44be170
sandeepn18 <=>
2025-01-28 23:05:18 +0530

    thread using thread class

--- Main.java
+++ Main.java
@@ -1,19 +1,44 @@
-class A{
-    void show(){
-        System.out.println("in A");
+class A extends Thread{
+    public void run(){
+        for(int i=0; i<=100; i++){
+            System.out.println("hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
-class B extends A{
-    void show(){
-        System.out.println("in B");
+class B extends Thread{
+    public void run(){
+        for(int i=0; i<=100; i++){
+            System.out.println("hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
-
 }
 public class Main{
     public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
 
-        A a = new B();
-        a.show();
+        a.start();
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        b.start();
+
+      
         
     }
 
